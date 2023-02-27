9ce21636b714a129461378ed3135ed381881721e
Silpashree <silpashree@bootlabstech.com>
2023-02-27 12:01:49 +0530

    rem jdbc

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
-  name          = var.glue_name
+  name          = var.glue_catalog_table_name
   database_name = aws_glue_catalog_database.catalog_database.name
 }
 resource "aws_glue_catalog_database" "catalog_database" {
-  name = var.database_name
+  name = var.glue_catalog_database_name
 }
-resource "aws_glue_connection" "glue_connection" {
-  connection_properties = {
-    JDBC_CONNECTION_URL = var.jdbc_connection_url
-    PASSWORD            = var.password
-    USERNAME            = var.username
-  }
-  name = var.glue_connection_name
-}
+# resource "aws_glue_connection" "glue_connection" {
+#   connection_properties = {
+#     JDBC_CONNECTION_URL = var.jdbc_connection_url
+#     PASSWORD            = var.password
+#     USERNAME            = var.username
+#   }
+#   name = var.glue_connection_name
+# }
--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
-variable "glue_name" {
+variable "glue_catalog_table_name" {
   description = "value"
   type        = string
 }
-variable "database_name" {
+variable "glue_catalog_database_name" {
   description = "value"
   type        = string
 }
-variable "jdbc_connection_url" {
-  description = "value"
-  type        = string
-}
-variable "password" {
-  description = "value"
-  type        = string
-}
-variable "username" {
-  description = "value"
-  type        = string
-}
-variable "glue_connection_name" {
-  description = "value"
-  type        = string
-}
+# variable "jdbc_connection_url" {
+#   description = "value"
+#   type        = string
+# }
+# variable "password" {
+#   description = "value"
+#   type        = string
+# }
+# variable "username" {
+#   description = "value"
+#   type        = string
+# }
+# variable "glue_connection_name" {
+#   description = "value"
+#   type        = string
+# }
 variable "region" {
   description = "AWS Region the instance is launched in"
   type        = string
